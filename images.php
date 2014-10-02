96eae04cf88a3538acb90a0cfa5bc16836865b61
Ryan Sadio <ryan@sadio.ca>
2014-10-01 19:46:01 -0700

    Formatting and cleanup code

--- images.php
+++ images.php
@@ -12,21 +12,22 @@ $imageFiles[] = array();
  * and all it's subdirectory.
  * A recursive function that calls itself when
  * entering another folder.
+ * @param $currDir current directory to scan
  * @param $imageFiles Array containing paths of files
  * @return array Array containing paths of files
  */
-function listFolderFiles( $dir, &$imageFiles )
+function listFolderFiles( $currDir, &$imageFiles )
 {
-    $folderFiles = scandir( $dir );
+    $folderFiles = scandir( $currDir );
     foreach ( $folderFiles as $folderFile ) {
         // Disregard current directory and 'up' directory
         if ( $folderFile != '.' && $folderFile != '..' ) {
             // Add  file path, name, and extension to the array
             // excluding folders
-            if ( !is_dir( $dir . '/' . $folderFile ) )
-                $imageFiles[] = $dir . '/' . $folderFile;
+            if ( !is_dir( $currDir . '/' . $folderFile ) )
+                $imageFiles[] = $currDir . '/' . $folderFile;
             else
-                listFolderFiles( $dir . '/' . $folderFile, $imageFiles );
+                listFolderFiles( $currDir . '/' . $folderFile, $imageFiles );
         }
     }
     return $imageFiles;
@@ -34,19 +35,22 @@ function listFolderFiles( $dir, &$imageFiles )
 
 /**
  * @param $imageFiles
+ * @return array filtered list of images
  */
 function FilterFileList( $imageFiles )
 {
     $targetFiles = array();
     $imgExt = "/.*\\.(jpg|jpeg|png|gif|bmp|tif|tiff)/i";
-    foreach ( $imageFiles as $imageFile ) {
-        if( !is_array( $imageFile ) && preg_match( $imgExt, $imageFile ) ) {
+    foreach ( $imageFiles as $imageFile )
+        if( !is_array( $imageFile ) && preg_match( $imgExt, $imageFile ) )
             $targetFiles[] = $imageFile;
-            echo ' <img src=" ' . $imageFile . ' " width="400"/><br />';
-        }
-    }
-/*    var_dump( $targetFiles );*/
+    return $targetFiles;
 }
 
-$fileList = listFolderFiles( 'images', $imageFiles );
-$filteredFileList = FilterFileList( $imageFiles );
+$filteredFileList = FilterFileList( listFolderFiles( $dir, $imageFiles ) );
+
+/* Show image and ask for input */
+foreach ($filteredFileList as $photo) {
+    echo ' <img src=" ' . $photo . ' " width="400"/><br />';
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>PHP - Lab04</title>
+</head>
+<body>
+
+<div id="photo" class="">Hello</div>
+<style>
+    #photo { background-image: url(<?php echo $photo;?>); }
+</style>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,10 +18,15 @@
  * Date: 29/09/2014
  * Time: 11:13 AM
  */
-$dir = './images';
+$dir = 'images';
 if (file_exists($dir)) {
     require_once 'images.php';
 }
 else {
     echo "there are no images";
 }
+
+
+?>
+</body>
+</html>
